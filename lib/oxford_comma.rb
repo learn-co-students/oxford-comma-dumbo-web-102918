6849544768849940adfafbfa3171784bdfa5ba89
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-15 19:18:09 +0000

    Oxford Comma Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    one_element = array.join
+    return one_element
+  elsif array.length == 2
+    two_elements = array.join(" and ")
+    return two_elements
+  else array.length >= 3
+    last_fruit = "#{array[-1]}"
+    array.insert(-2, "and")
+    array.pop
+    more_than_two = array.join(", ")
+    return "#{more_than_two} #{last_fruit}"
+  end
 end
