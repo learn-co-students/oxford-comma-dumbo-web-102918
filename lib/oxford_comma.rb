95c01830fc028ec76bb683abc996dd670240ea12
Eizik Gottesfeld <ezkmonsey@gmail.com>
2018-10-18 20:32:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-new_1= ""
-for i in 0..array.length-1 do 
-  new << array.join(,)
-end 
-new_1 << array.last
-new_1
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
