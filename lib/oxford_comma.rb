fe6604ca53c38a7b136b47dc50e8250da4249c3f
naomijoyce <naomijoyceb@gmail.com>
2018-10-22 11:28:41 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+#array.join
+
+
+#array.join(" and ")
+
+#array[0..-2].join(", ") << ", and " << array[-1]
+
+if array.count >= 3
+  array[0..-2].join(", ") << ", and " << array[-1]
+elsif array.count > 1
+  array.join(" and ")
+else
+  array.join
+end
 
 end
