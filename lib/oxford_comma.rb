168f3437a65507c24333d4919f13e639d46f5dfc
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-23 17:12:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,31 @@
 def oxford_comma(array)
-
+ length = array.length
+    if(length == 1)
+      array.join
+    elsif (length == 2) 
+      array.join(" and ")
+    elsif (length == 3)
+      three_string = ""
+      three_string << array[0]
+      three_string << ", "
+      three_string << array[1]
+      three_string << ", and "
+      three_string << array[2]
+        three_string
+    else
+      more_string=""
+      counter = 0 
+        while(counter < length)
+          more_string << array[counter]
+            if(counter == length-2)
+              more_string << ", and "
+            else
+              more_string << ", "
+            end  
+          counter += 1
+        end
+         more_string.chop.chop
+    end
 end
+
+
