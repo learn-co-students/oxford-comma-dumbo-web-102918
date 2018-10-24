b901589aca920b6d8c8be3e6dd3b3220a22de466
mwestervelt <elle.westervelt@gmail.com>
2018-10-23 20:06:24 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return array.join(" and ")
+  elsif array.length > 2
+     array[-2] << (", and")
+     return array.join (", ")
+  end
+  array.join(", ")
 end
