a16f425b94dea917fd94d90274bd6576a7c1a9a5
a lan <bluenowte97@gmail.com>
2018-11-06 18:56:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
 
+if array.length == 1
+  string = array.join
+
+elsif array.length == 2
+    array.insert(-2,"and")
+    string = array.join(" ")
+
+  else array.length > 2
+
+    array.insert(-2,"and")
+    string = array.join(", ")
+    string.gsub('and,', 'and')
+    #gsub takes two arguments. The first is the text you want to replace
+    #and the second is the new text.
+
+  end
 end
